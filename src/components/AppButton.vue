ef0c265d4e23559f6ec91e9f1182faa13b3f4082
githukelvin <kelvingithu019@gmail.com>
2023-08-11 17:57:19 +0300

    navbar done finalizing on header component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <a class="btn" :href="link">{{text}}</a>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  text: String,
+  link: String
+})
+</script>
+
+<style lang="scss" scoped>
+.btn{
+  padding: .9em 2.5em;
+  margin-right: -8px;
+  background: #fff;
+  border-radius: 35px;
+}
+</style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,29 +1,64 @@
 <template>
-<header>
-  <div class="container ">
-  <div class="content navBar">
-    <div class="left">
-      <a href="/"><img src="/images/logo.svg" alt=""></a>
-      <nav>
-        <ul>
-          <li><a href="/product">product<div></div></a></li>
-          <li><a href="/company">company<div></div></a></li>
-          <li><a href="/connect">connect<div></div></a></li>
-        </ul>
-      </nav>
+  <header>
+    <div class="container">
+      <div class="content navBar">
+        <div class="left">
+          <a href="/"><img src="/images/logo.svg" alt="" /></a>
+          <nav>
+            <ul>
+              <li>
+                <a href="/product">product</a>
+                <div></div>
+              </li>
+              <li>
+                <a href="/company">company</a>
+                <div></div>
+              </li>
+              <li>
+                <a href="/connect">connect</a>
+                <div></div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="right btns">
+          <a href="#">login</a>
+          <AppButton link="#" text="Sign up" />
+        </div>
+      </div>
     </div>
-    <div class="right btns">
-
+    <div class="middleContent">
+      <h1>A modern publishing platform</h1>
+      <p>Grow your audience and build your online brand</p>
+      <div class="btns">
+        <AppButton link="#" text="Start for Free" />
+        <a href="#">Learn More</a>
+      </div>
     </div>
-  </div>
-</div>
-</header>
+  </header>
 </template>
 
 <script setup>
-
+import AppButton from './AppButton.vue'
 </script>
 
 <style lang="scss" scoped>
+.middleContent {
+  width: fit-content;
+  margin-inline: auto;
+  margin-top: 8em;
 
+  h1,
+  p {
+    text-align: center;
+    color: #fff;
+  }
+  h1 {
+    font-size: 3.65rem;
+  }
+  p {
+    font-size: 1.2rem;
+    padding: 1.5em 0;
+  }
+}
 </style>
