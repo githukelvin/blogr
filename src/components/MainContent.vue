28ac9508ab8effcac950602dd9e3fa526acc5e23
githukelvin <kelvingithu019@gmail.com>
2023-08-14 22:43:16 +0300

    been busy revisiting the prject

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -24,7 +24,15 @@
         </div>
       </section>
       <section class="sect2">
-        <div class="content">
+        <div class="content details">
+          <div class="dleft">
+            <img src="/images/illustration-phones.svg" alt="phones illusrations">
+          </div>
+          <div class="dright">
+            <h1> State of the Art Infrastructure</h1>
+            <p> With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
+    This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
+          </div>
         </div>
       </section>
    </div>
@@ -59,6 +67,38 @@ import AppPara from './AppPara.vue'
   border-bottom-left-radius: 100px;
   height: 400px;
   border-top-right-radius: 100px;
+  background-position-y: -500px;
+  background-position-x: -210px;
+  .details{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 3.9em;
+  }
+  .dleft{
+    margin-top:-4.3em;
+    margin-left:-2.5em;
+  }
+  .dright{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 7.3em;
+    h1{
+      margin-left: -1.23em;
+      text-align: start;
+      font-size: 2.3em;
+      margin-bottom: 0;
+      padding-bottom: 0;
+    }
+    p{
+    line-height: 1.74;
+    width: 100%;
+    word-spacing: 2px;
+    letter-spacing: .2px;
+      margin-left: -1.85em;
+
+  }
+  }
 }
 }
 
