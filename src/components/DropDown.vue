e98a29c73a6f99eb0670f46bc231bfb6cde6e8ec
githukelvin <kelvingithu019@gmail.com>
2023-08-15 08:52:35 +0300

    dropdown implemented

---
+++ src/components/DropDown.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dropdown">
+    <ul>
+      <li>
+        <a href="">{{ link1 }}</a>
+      </li>
+      <li>
+        <a href="">{{ link2 }}</a>
+      </li>
+      <li>
+        <a href="">{{ link3 }}</a>
+      </li>
+      <li>
+        <a href="">{{ link4 }}</a>
+      </li>
+      <li>
+        <a href="">{{ link5 }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  link1: String,
+  link2: String,
+  link3: String,
+  link4: String,
+  link5: String
+})
+</script>
+
+<style lang="scss" scoped>
+.dropdown{
+  background: #fff;
+  border-radius: 5px;
+  padding: 1.5em 2em;
+  margin-top: 1em;
+  width: 220px;
+ ul{
+   display: flex;
+  flex-direction: column;
+ }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,63 @@
-<template>
-   <div class="content navBar">
-          <div class="left">
-            <a href="/"><img src="/images/logo.svg" alt="" /></a>
-            <nav>
-              <ul>
-                <li>
-                  <a href="/product">product</a>
-                  <div></div>
-                </li>
-                <li>
-                  <a href="/company">company</a>
-                  <div></div>
-                </li>
-                <li>
-                  <a href="/connect">connect</a>
-                  <div></div>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div class="right btns">
-            <a href="#">login</a>
-            <AppButton link="#" text="Sign up" />
-          </div>
-        </div>
-</template>
-
 <script setup>
+import DropDown from './DropDown.vue'
 import AppButton from './AppButton.vue'
-
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <div class="content navBar">
+    <div class="left">
+      <a href="/"><img src="/images/logo.svg" alt="" /></a>
+      <nav>
+        <ul>
+          <div class="li1">
+            <li>
+              <a href="/product">product</a>
+              <div id="bg"></div>
+            </li>
+            <div class="showdropdown">
+              <DropDown
+                link1="Overview"
+                link2="Pricing"
+                link3="Marketplace"
+                link4="Features"
+                link5="Integrations"
+              />
+            </div>
+          </div>
+          <div class="li2">
+            <li>
+              <a href="/company">company</a>
+              <div id="bg"></div>
+            </li>
+            <div class="showdropdown">
+              <DropDown
+                link1="About"
+                link2="Team"
+                link3="Blog"
+                link4="Careers"
+              />
+            </div>
+          </div>
+          <div class="li3">
+            <li>
+              <a href="/connect">connect</a>
+              <div id="bg"></div>
+            </li>
+            <div class="showdropdown">
+              <DropDown
+              link1="Contact"
+              link2="Newsletter"
+               link3="LinkedIn" />
+            </div>
+          </div>
+        </ul>
+      </nav>
+    </div>
+    <div class="right btns">
+      <a href="#">login</a>
+      <AppButton link="#" text="Sign up" />
+    </div>
+  </div>
+</template>
 
-</style>
+<style lang="scss" scoped></style>
